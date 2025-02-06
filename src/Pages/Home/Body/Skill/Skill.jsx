cad110494e1cc0282ad8efc238d24a8a371aef87
mazharul90007 <mazharul90007@gmail.com>
2025-02-06 12:17:30 +0600

    Soft Skills added

--- src/Pages/Home/Body/Skill/Skill.jsx
+++ src/Pages/Home/Body/Skill/Skill.jsx
@@ -1,4 +1,4 @@
-import { FaHtml5, FaCss3Alt, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaJs, FaNodeJs, FaReact, FaCheck } from "react-icons/fa";
 import { SiTailwindcss, SiBootstrap, SiFirebase, SiExpress, SiMongodb, SiDaisyui } from "react-icons/si";
 import ProgressBar from "@ramonak/react-progress-bar";
 
@@ -17,37 +17,58 @@ const Skill = () => {
         { name: "MongoDB", icon: <SiMongodb className="text-green-600 text-2xl" />, progress: 70, color: "#16A34A" },
     ];
 
+    const softSkills = [
+        {skill: 'Comunication Skills'},
+        {skill: 'Problem Solving'},
+        {skill: 'Teamwork & Collaboration'},
+        {skill: 'Adaptability & Continuous Learning'},
+        {skill: 'Attention to Detail'},
+    ]
+
     return (
         <div className="my-8 p-8 bg-secondary">
-            <div>
-                <h2 className="text-2xl font-robotoSlab mb-5 font-semibold border-b-2 py-3">
-                    <span className="text-primary">S</span>kills
-                </h2>
-            </div>
-            <section className="grid md:grid-cols-2">
-                {/* Technical Skills */}
-                <div className="grid md:grid-cols-2 md:col-span-1 gap-2 mb-16">
-                    {skills.map((skill, index) => (
-                        <div key={index} className="text-center p-4 shadow rounded-md bg-[#fcd2c3] w-full">
-                            <div className="flex items-center justify-center mb-2">
-                                {skill.icon}
-                                <p className="text-gray-500 font-medium ml-2">{skill.name}</p>
-                            </div>
-                            <div className="w-full">
-                                <ProgressBar
-                                    completed={skill.progress}
-                                    bgColor={skill.color}
-                                    width="100%"
-                                    height={8}
-                                    isLabelVisible={false} // Hides percentage text
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                {/* Soft Skills */}
+            <section className="grid md:grid-cols-2 gap-4">
                 <div className="md:col-span-1">
+                    <h2 className="text-2xl font-robotoSlab mb-2 font-semibold border-b-2 py-3">
+                        <span className="text-primary">T</span>ech Skills
+                    </h2>
+                    <div className="grid md:grid-cols-2  gap-2 mb-16">
+                        {skills.map((skill, index) => (
+                            <div key={index} className="text-center p-4 shadow rounded-md bg-[#fcd2c3] w-full">
+                                <div className="flex items-center justify-center mb-2">
+                                    {skill.icon}
+                                    <p className="text-gray-500 font-medium ml-2">{skill.name}</p>
+                                </div>
+                                <div className="w-full">
+                                    <ProgressBar
+                                        completed={skill.progress}
+                                        bgColor={skill.color}
+                                        width="100%"
+                                        height={8}
+                                        isLabelVisible={false} // Hides percentage text
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {/* Technical Skills */}
 
+                {/* Soft Skills */}
+                <div className="md:col-span-1 pl-5 md:border-l">
+                    <h2 className="text-2xl font-robotoSlab mb-2 font-semibold border-b-2 py-3">
+                        <span className="text-primary">S</span>oft Skills
+                    </h2>
+                    <div className="space-y-2">
+                        {
+                            softSkills.map((softSkill,index) =>
+                                <div key={index} className="flex items-center gap-2">
+                                    <span className="text-primary"><FaCheck /></span>
+                                    <p className="font-semibold text-gray-500">{softSkill.skill}</p>
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
             </section>
         </div>
