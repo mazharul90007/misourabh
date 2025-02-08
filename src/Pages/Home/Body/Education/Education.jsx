0142eb9f1e4006be327f367c501956fb13072dd4
mazharul90007 <mazharul90007@gmail.com>
2025-02-08 13:26:55 +0600

    Course and Certification section added in education section

--- src/Pages/Home/Body/Education/Education.jsx
+++ src/Pages/Home/Body/Education/Education.jsx
@@ -1,4 +1,4 @@
-import { FaGraduationCap, FaUniversity } from "react-icons/fa";
+import { FaAward, FaGraduationCap, FaUniversity } from "react-icons/fa";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -26,52 +26,133 @@ const educationData = [
   },
 ];
 
+const certificationData = [
+  {
+    id: 1,
+    courseName: 'Complete Web Development Course',
+    institution: 'Programming Hero',
+    period: 'June 2024 to January 2025',
+    address:'Online',
+    icon: <FaAward />
+  },
+  {
+    id: 2,
+    courseName: 'Professional Web Design',
+    institution: 'BASIS SEIP ',
+    period: '9th April 2023 to 25th June 2023',
+    address:'Karwan Bazar, Dhaka, Bangladesh',
+    icon: <FaAward />
+  },
+  {
+    id: 3,
+    courseName: 'Professional Web Design and Development ',
+    institution: 'UY LAB ',
+    period: '30th January 2023 to 24th July 2023',
+    address:'Wireless Gate Rd, Mohakhali, Dhaka, Bangladesh',
+    icon: <FaAward />
+  },
+  {
+    id: 4,
+    courseName: 'Professional English Communication Skills Certification',
+    institution: 'Workplace Skills Development Academy New Zealand (WSDA)',
+    period: '18th June 2023',
+    address:'BITM',
+    icon: <FaAward />
+  },
+]
+
 const Education = () => {
   return (
-    <div className="bg-secondary py-4 md:py-8" id="education">
-      <div className="px-4 md:px-8">
-        <Title firstLetter={'E'} title={'ducation'}></Title>
-      </div>
-      <div>
-        <VerticalTimeline lineColor="#F6A98F" layout="1-column-left" animate>
-          {educationData.map((edu) => (
-            <VerticalTimelineElement
-              key={edu.id}
-              className="vertical-timeline-element--education"
-              contentStyle={{ textAlign: "left" }}
-              contentArrowStyle={{ borderRight: '7px solid  white' }} // Hides the arrow
-              iconStyle={{ background: "#F6A98F", color: "#fff" }}
-              icon={edu.icon}
-              position="right" // Aligns all items on the right side
-            >
-              <h2 className="text-xl text-gray-500 font-semibold">
-                {edu.degree}
-              </h2>
-              <div className="space-y-1">
-                <p className="text-gray-400">
-                  <span className="text-gray-500 font-semibold">
-                    Institution:
-                  </span>{" "}
-                  {edu.institution}
-                </p>
-                <p className="text-gray-400">
-                  <span className="text-gray-500 font-semibold">Year:</span>{" "}
-                  {edu.year}
-                </p>
-                {edu.group && (
+    <div className="bg-secondary py-4 md:py-8 grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-0" id="education">
+
+      {/* Education */}
+      <div className="md:col-span-1">
+        <div className="px-4 md:pl-8">
+          <Title firstLetter={'E'} title={'ducation'}></Title>
+        </div>
+        <div>
+          <VerticalTimeline lineColor="#F6A98F" layout="1-column-left" animate>
+            {educationData.map((edu) => (
+              <VerticalTimelineElement
+                key={edu.id}
+                className="vertical-timeline-element--education"
+                contentStyle={{ textAlign: "left" }}
+                contentArrowStyle={{ borderRight: '7px solid  white' }} // Hides the arrow
+                iconStyle={{ background: "#F6A98F", color: "#fff" }}
+                icon={edu.icon}
+                position="right" // Aligns all items on the right side
+              >
+                <h2 className="text-xl text-gray-500 font-semibold">
+                  {edu.degree}
+                </h2>
+                <div className="space-y-1">
                   <p className="text-gray-400">
-                    <span className="text-gray-500 font-semibold">Group:</span>{" "}
-                    {edu.group}
+                    <span className="text-gray-500 font-semibold">
+                      Institution:
+                    </span>{" "}
+                    {edu.institution}
                   </p>
-                )}
-                <p className="text-gray-400">
-                  <span className="text-gray-500 font-semibold">Address:</span>{" "}
-                  {edu.address}
-                </p>
-              </div>
-            </VerticalTimelineElement>
-          ))}
-        </VerticalTimeline>
+                  <p className="text-gray-400">
+                    <span className="text-gray-500 font-semibold">Year:</span>{" "}
+                    {edu.year}
+                  </p>
+                  {edu.group && (
+                    <p className="text-gray-400">
+                      <span className="text-gray-500 font-semibold">Group:</span>{" "}
+                      {edu.group}
+                    </p>
+                  )}
+                  <p className="text-gray-400">
+                    <span className="text-gray-500 font-semibold">Address:</span>{" "}
+                    {edu.address}
+                  </p>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
+      {/* Certification */}
+      <div className="md:col-span-1 md:border-l">
+        <div className="text-2xl font-robotoSlab mb-5 font-semibold border-b-2 pb-3 ml-5">
+          <span className="text-primary">C</span>ourses & <span className="text-primary">C</span>ertifications
+        </div>
+        <div>
+          <VerticalTimeline lineColor="#F6A98F" layout="1-column-left" animate>
+            {certificationData.map((certificate) => (
+              <VerticalTimelineElement
+                key={certificate.id}
+                className="vertical-timeline-element--education"
+                contentStyle={{ textAlign: "left" }}
+                contentArrowStyle={{ borderRight: '7px solid  white' }} // Hides the arrow
+                iconStyle={{ background: "#F6A98F", color: "#fff" }}
+                icon={certificate.icon}
+                position="right" // Aligns all items on the right side
+              >
+                <h2 className="text-xl text-gray-500 font-semibold">
+                  {certificate.courseName}
+                </h2>
+                <div className="space-y-1">
+                  <p className="text-gray-400">
+                    <span className="text-gray-500 font-semibold">
+                      Institution:
+                    </span>{" "}
+                    {certificate.institution}
+                  </p>
+                  <p className="text-gray-400">
+                    <span className="text-gray-500 font-semibold">Duration:</span>{" "}
+                    {certificate?.period}
+                  </p>
+                  <p className="text-gray-400">
+                    <span className="text-gray-500 font-semibold">Address:</span>{" "}
+                    {certificate.address}
+                  </p>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
       </div>
     </div>
   );
