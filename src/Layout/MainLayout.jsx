d56dba6c6a3820112c2809f585b995a2699d08e5
mazharul90007 <mazharul90007@gmail.com>
2025-02-27 22:10:04 +0600

    main layout max width correction hasbeen fixed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -5,25 +5,27 @@ import Topbar from "../Components/Topbar/Topbar";
 
 const MainLayout = () => {
     return (
-        <div className="bg-paper max-w-7xl mx-auto font-roboto">
-            <div className="sticky top-0 z-50">
-                <Topbar></Topbar>
-            </div>
-            {/* Sidebar & Content */}
-            <div className="grid md:grid-cols-12 gap-1 w-[98%]  md:w-[96%] mx-auto">
-                {/* Sticky Sidebar */}
-                <div className="md:col-span-2 mx-auto md:h-[calc(100vh-130px)] sticky top-12 md:top-16 lg:top-32 overflow-y-auto z-30">
-                    <Sidebar />
+        <div className="bg-paper  font-roboto">
+            <div className="max-w-7xl mx-auto">
+                <div className="sticky top-0 z-50">
+                    <Topbar></Topbar>
+                </div>
+                {/* Sidebar & Content */}
+                <div className="grid md:grid-cols-12 gap-1 w-[98%]  md:w-[96%] mx-auto">
+                    {/* Sticky Sidebar */}
+                    <div className="md:col-span-2 mx-auto md:h-[calc(100vh-130px)] sticky top-12 md:top-16 lg:top-32 overflow-y-auto z-30">
+                        <Sidebar />
+                    </div>
+
+                    {/* Scrollable Content */}
+                    <div className="md:col-span-10 min-h-[calc(100vh-288px)] overflow-y-auto">
+                        <Outlet />
+                    </div>
                 </div>
 
-                {/* Scrollable Content */}
-                <div className="md:col-span-10 min-h-[calc(100vh-288px)] overflow-y-auto">
-                    <Outlet />
-                </div>
+                {/* Footer */}
+                <Footer />
             </div>
-
-            {/* Footer */}
-            <Footer />
         </div>
     );
 };
